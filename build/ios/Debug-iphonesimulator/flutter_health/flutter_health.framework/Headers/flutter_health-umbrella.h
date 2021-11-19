821db672cf65f9c011b28790df048b5be85c3101
Jonathan Lifferth <jonathan.lifferth@gmail.com>
2021-11-18 23:01:21 -0600

    initial commit with functioning ios healthkit permission request

---
+++ build/ios/Debug-iphonesimulator/flutter_health/flutter_health.framework/Headers/flutter_health-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "FlutterHealthPlugin.h"
+
+FOUNDATION_EXPORT double flutter_healthVersionNumber;
+FOUNDATION_EXPORT const unsigned char flutter_healthVersionString[];
+
